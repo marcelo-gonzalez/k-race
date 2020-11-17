86632e1215a16ae5757e88864d03187ea6267b5e
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2020-11-17 11:17:20 -0500

    print records as we find them in examine.py
    reading all of it to memory first = bad

--- examine.py
+++ examine.py
@@ -30,59 +30,77 @@ def single_datapoint_len(num_params):
     return num_params*8+4+4
 
 
-def read_lines(f, lines, data_fmt, num_params):
-    data = []
+def foreach_record(file, data_fmt, num_params, f, lines):
+    i = 0
     while True:
-        if lines > 0 and len(data) >= lines:
-            break
+        if lines > 0 and i >= lines:
+            return
 
-        x = f.read(single_datapoint_len(num_params))
+        x = file.read(single_datapoint_len(num_params))
         if len(x) < single_datapoint_len(num_params):
-            break
-        data.append(struct.unpack(data_fmt, x))
-    return data
+            return
+        f(struct.unpack(data_fmt, x))
+        i += 1
 
-def read_k_race_file(filename, lines=0):
-    with open(filename, 'rb') as f:
-        magic = f.read(len('k_race_data'))
-        if magic != b'k_race_data':
-            raise ValueError('%s does not appear to be a k-race output file' % filename)
 
-        nump = f.read(4)
-        num_params = struct.unpack('<I', nump)[0]
+def k_race_file_parse_header(filename, file):
+    magic = file.read(len('k_race_data'))
+    if magic != b'k_race_data':
+        raise ValueError('%s does not appear to be a k-race output file' % filename)
 
-        # little endian
-        data_fmt = '<'
-        # signed 64 bits for each param
-        for i in range(num_params):
-            data_fmt += 'q'
+    nump = file.read(4)
+    num_params = struct.unpack('<I', nump)[0]
+
+    # little endian
+    data_fmt = '<'
+    # signed 64 bits for each param
+    for i in range(num_params):
+        data_fmt += 'q'
         # unsigned 32 bits for counts and triggers
-        data_fmt += 'II'
+    data_fmt += 'II'
+    return data_fmt, num_params
 
-        if lines >= 0:
-            data = read_lines(f, lines, data_fmt, num_params)
-            index_start = 0
-        else:
-            lines = -lines
-            start = f.seek(0, os.SEEK_CUR)
-            end = f.seek(0, os.SEEK_END)
 
-            p = end - lines * single_datapoint_len(num_params)
-            if p < start:
-                p = start
-            f.seek(p, os.SEEK_SET)
-            data = read_lines(f, lines, data_fmt, num_params)
-            index_start = (p-start)/single_datapoint_len(num_params)
+def k_race_file_foreach_record(file, f, data_fmt, num_params, lines):
+    if lines >= 0:
+        data = foreach_record(file, data_fmt, num_params, f, lines)
+        index_start = 0
+    else:
+        lines = -lines
+        start = file.seek(0, os.SEEK_CUR)
+        end = file.seek(0, os.SEEK_END)
 
-        columns = []
-        for i in range(num_params):
-            columns.append('offset_%d' % i)
-        columns += ['counts', 'triggers']
-        ret = pd.DataFrame(data, columns=columns,
-                           index=pd.RangeIndex(index_start, index_start + len(data)))
-        ret['triggers'] /= ret['counts']
-        ret.fillna(value=0, inplace=True)
-        return ret
+        p = end - lines * single_datapoint_len(num_params)
+        if p < start:
+            p = start
+        file.seek(p, os.SEEK_SET)
+        data = foreach_record(file, data_fmt, num_params, f, lines)
+        index_start = (p-start)/single_datapoint_len(num_params)
+    return index_start
+
+
+def read_k_race_file(filename, file, data_fmt, num_params, columns):
+    data = []
+    def fill_data(record):
+        data.append(record)
+    start = k_race_file_foreach_record(file, fill_data, data_fmt, num_params, 100000)
+
+    ret = pd.DataFrame(data, columns=columns,
+                       index=pd.RangeIndex(start, start + len(data)))
+    ret['triggers'] /= ret['counts']
+    ret.fillna(value=0, inplace=True)
+    return ret
+
+
+def print_k_race_file(filename, file, data_fmt, num_params, columns, lines):
+    fmt = '{:>10}'*(num_params+2)
+    print(fmt.format(*columns))
+    def print_record(record):
+        triggers = float(record[-1]) / float(record[-2])
+        print(fmt.format(*record[:-1], triggers))
+
+    k_race_file_foreach_record(file, print_record, data_fmt, num_params, lines)
+
 
 def main():
     parser = argparse.ArgumentParser(description='display k-race output')
@@ -106,20 +124,28 @@ def main():
         print('the argument given with -n must be positive', file=sys.stderr)
         sys.exit(1)
 
-    if args.cmd == 'head':
-        data = read_k_race_file(args.file, args.n)
-    elif args.cmd == 'tail':
-        data = read_k_race_file(args.file, -args.n)
-    else:
-        data = read_k_race_file(args.file)
+    file = open(args.file, 'rb')
+    data_fmt, num_params = k_race_file_parse_header(args.file, file)
+    columns = []
+    for i in range(num_params):
+        columns.append('offset_%d' % i)
+    columns += ['counts', 'triggers']
 
     if args.cmd == 'plot':
+        data = read_k_race_file(args.file, file, data_fmt, num_params, columns)
         fig = plt.figure()
         add_plot(fig, data)
         plt.show()
     else:
-        with pd.option_context('display.max_rows', None):
-            print(data)
+        n = 0
+        if args.cmd == 'tail':
+            n = -args.n
+        elif args.cmd == 'head':
+            n = args.n
+        print_k_race_file(args.file, file, data_fmt, num_params, columns, n)
+
+    file.close()
+
 
 if __name__ == '__main__':
     main()
